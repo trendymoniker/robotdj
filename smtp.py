bd9d9475c816de1b4409e8a590dfa6fada1b4237
Kyle Benson <kebenson@uci.edu>
2012-08-29 00:08:28 -0700

    merging some minor changes, mostly syntax

--- smtp.py
+++ smtp.py
@@ -3,6 +3,8 @@ from twisted.mail import imap4
 from twisted.python import log, failure
 from get_username_password import *
 
+SUBJECT_INDEX = 1
+
 @defer.inlineCallbacks
 def main(username="alice", password="secret",
          strport="ssl:host=imap.gmail.com:port=993"):
@@ -13,10 +15,13 @@ def main(username="alice", password="secret",
         client = yield endpoint.connect(factory)
         yield client.login(username, password)
         yield client.select('INBOX')
+        info = yield client.fetchEnvelope(imap4.MessageSet(1))
 
         #print subjects for all new emails
-        info = yield client.fetchEnvelope(imap4.MessageSet(1))
-        print info[1]['ENVELOPE'][1]
+        #print info[1]['ENVELOPE'][SUBJECT_INDEX]
+        
+        print (i for i in info['ENVELOPE'])
+
     except:
         log.err(failure.Failure(), "IMAP4 client interaction failed")
     reactor.stop()
